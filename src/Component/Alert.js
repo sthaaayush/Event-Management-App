61ee8ea14a1e5cf1f5127c12e28ac59d67204f89
Aayush Shrestha <aayushstha20@gmail.com>
2025-08-02 17:37:12 +0545

    updated alert to appear in middle

--- src/Component/Alert.js
+++ src/Component/Alert.js
@@ -1,14 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-export default function Alert({message, type}) {
+export default function Alert({ message, type }) {
     return (
-        <div>
-            {
-                message &&
-                <div class={`alert alert-${type}`} role="alert">
+        <>
+            {message && (
+                <div
+                    className={`alert alert-${type} position-fixed top-50 start-50 translate-middle text-center shadow`}
+                    role="alert"
+                    style={{ zIndex: 1050, minWidth: "300px", maxWidth: "80vw" }}
+                >
                     {message}
                 </div>
-            }
-        </div>
-    )
+            )}
+        </>
+    );
 }
